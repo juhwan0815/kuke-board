ad9c36cc80b3e28cc68d4985d9272f0a10d62d93
hwangjuhwan <jhanhwang@kicc.co.kr>
2025-03-12 16:46:45 +0900

    스프링부트로 직접 만들면서 배우는 대규모 시스템 설계 - 게시판 : 게시글

---
+++ service/article/src/test/java/kuke/board/article/api/ArticleApiTest.java
@@ -0,0 +1,130 @@
+package kuke.board.article.api;
+
+
+import kuke.board.article.service.response.ArticlePageResponse;
+import kuke.board.article.service.response.ArticleResponse;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+
+public class ArticleApiTest {
+
+    RestClient restClient = RestClient.create("http://localhost:9000");
+
+    @Test
+    void createTest() {
+        ArticleResponse response = create(new ArticleCreateRequest(
+                "hi", "my content", 1L, 1L
+        ));
+
+        System.out.println("response = " + response);
+    }
+
+    @Test
+    void readTest() {
+        ArticleResponse response = read(158040293770166272L);
+        System.out.println("response = " + response);
+    }
+
+    @Test
+    void update() {
+        update(158040293770166272L);
+        ArticleResponse response = read(158040293770166272L);
+        System.out.println("response = " + response);
+    }
+
+    @Test
+    void delete() {
+        restClient.delete()
+                .uri("/v1/articles/{articleId}", 158040293770166272L)
+                .retrieve();
+    }
+
+    @Test
+    void readAllTest() {
+        ArticlePageResponse response = restClient.get()
+                .uri("/v1/articles?boardId=1&pageSize=30&page=50000")
+                .retrieve()
+                .body(ArticlePageResponse.class);
+
+        System.out.println("response.getArticleCount() = " + response.getArticleCount());
+        for (ArticleResponse article : response.getArticles()) {
+            System.out.println("articleId = " + article.getArticleId());
+        }
+    }
+    
+    @Test
+    void readAllInfiniteScrollTest() {
+        List<ArticleResponse> articles1 = restClient.get()
+                .uri("/v1/articles/infinite-scroll?boardId=1&pageSize=5")
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<ArticleResponse>>() {
+                });
+
+        System.out.println("firstPage");
+        for (ArticleResponse articleResponse : articles1) {
+            System.out.println("articleResponse.getArticleId() = " + articleResponse.getArticleId());
+        }
+
+        Long lastArticleId = articles1.getLast().getArticleId();
+        List<ArticleResponse> articles2 = restClient.get()
+                .uri("/v1/articles/infinite-scroll?boardId=1&pageSize=5&lastArticleId=%s".formatted(lastArticleId))
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<ArticleResponse>>() {
+                });
+
+        System.out.println("secondPage");
+        for (ArticleResponse articleResponse : articles2) {
+            System.out.println("articleResponse.getArticleId() = " + articleResponse.getArticleId());
+        }
+    }
+
+
+    void update(Long articleId) {
+        restClient.put()
+                .uri("/v1/articles/{articleId}", articleId)
+                .body(new ArticleUpdateRequest("hi2", "content2"))
+                .retrieve();
+    }
+
+    ArticleResponse read(Long articleId) {
+        return restClient.get()
+                .uri("/v1/articles/{articleId}", articleId)
+                .retrieve()
+                .body(ArticleResponse.class);
+    }
+
+
+    ArticleResponse create(ArticleCreateRequest request) {
+        return restClient.post()
+                .uri("/v1/articles")
+                .body(request)
+                .retrieve()
+                .body(ArticleResponse.class);
+    }
+
+
+    @Getter
+    @AllArgsConstructor
+    static class ArticleCreateRequest {
+
+        private String title;
+        private String content;
+        private Long writerId;
+        private Long boardId;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    static class ArticleUpdateRequest {
+
+        private String title;
+        private String content;
+    }
+
+
+}
