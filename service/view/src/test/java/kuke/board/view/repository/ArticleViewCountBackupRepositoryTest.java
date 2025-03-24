2701dfa25f0ceee219ff2a17938d5269c4393ebc
juhwan0815 <juwom0831@naver.com>
2025-03-24 14:09:56 +0900

    스프링부트로 직접 만들면서 배우는 대규모 시스템 설계 - 조회수

---
+++ service/view/src/test/java/kuke/board/view/repository/ArticleViewCountBackupRepositoryTest.java
@@ -0,0 +1,46 @@
+package kuke.board.view.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import kuke.board.view.entity.ArticleViewCount;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class ArticleViewCountBackupRepositoryTest {
+
+    @Autowired
+    ArticleViewCountBackupRepository articleViewCountBackupRepository;
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Test
+    @Transactional
+    void updateViewCountTest() {
+        // given
+        articleViewCountBackupRepository.save(
+                ArticleViewCount.init(1L, 0L));
+
+        entityManager.flush();
+        entityManager.clear();
+
+        // when
+        int result1 = articleViewCountBackupRepository.updateViewCount(1L, 100L);
+        int result2 = articleViewCountBackupRepository.updateViewCount(1L, 300L);
+        int result3 = articleViewCountBackupRepository.updateViewCount(1L, 200L);
+
+        // then
+        assertThat(result1).isEqualTo(1);
+        assertThat(result2).isEqualTo(1);
+        assertThat(result3).isEqualTo(0);
+
+        ArticleViewCount articleViewCount = articleViewCountBackupRepository.findById(1L).get();
+        assertThat(articleViewCount.getViewCount()).isEqualTo(300L);
+    }
+
+}
